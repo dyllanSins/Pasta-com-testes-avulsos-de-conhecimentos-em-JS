32c1f26da54855ce27311a12a14ce92bc8902fd6
Dyllan Barreto <123782326+dyllanSins@users.noreply.github.com>
2023-05-11 17:06:04 -0300

    Update and rename index.js to calculadoraOperacoesBasicas.js

--- calculadoraOperacoesBasicas.js
+++ calculadoraOperacoesBasicas.js
@@ -1,14 +1,14 @@
-const readline = require('readline');
+const readline = require('readline'); 
 
 const rl = readline.createInterface({
-    input: process.stdin,
+    input: process.stdin, //  criar uma interface de leitura e escrita no terminal, que será usada para coletar os dados de entrada do usuário.
     output: process.stdout 
 });
 
 console.log("Bem-vindo à calculadora em JavaScript!");
 
 rl.question("Digite o primeiro número: ", function(num1) {
-    rl.question("Digite o segundo número: ", function(num2) {
+    rl.question("Digite o segundo número: ", function(num2) {  //A função rl.question é usada para solicitar ao usuário que digite o primeiro número, o segundo número e a operação que deseja realizar. Cada resposta do usuário é passada para uma função de retorno de chamada que é executada quando o usuário pressiona "enter".
         rl.question("Digite a operação (+, -, *, /): ", function(operacao) {
 
             num1 = parseFloat(num1);
